caaa2e6807e9814c7d6a560085c99645bf6b1863
awells2 <awells2@stanford.edu>
2016-03-08 23:34:57 -0800

    db changes

--- db/structure.sql
+++ db/structure.sql
@@ -28,3 +28,12 @@ INSERT INTO schema_migrations (version) VALUES ('20150624174713');
 INSERT INTO schema_migrations (version) VALUES ('20150624171207');
 
 INSERT INTO schema_migrations (version) VALUES ('20150624174713');
+INSERT INTO schema_migrations (version) VALUES ('20150624171207');
+
+INSERT INTO schema_migrations (version) VALUES ('20150624174713');
+INSERT INTO schema_migrations (version) VALUES ('20150624171207');
+
+INSERT INTO schema_migrations (version) VALUES ('20150624174713');
+INSERT INTO schema_migrations (version) VALUES ('20150624171207');
+
+INSERT INTO schema_migrations (version) VALUES ('20150624174713');
