0bd71fb3095379aaf4945f1a2d0a582076fc01f5
awells2 <awells2@stanford.edu>
2016-03-09 21:54:09 -0800

    removed rinbuby

--- app/helpers/sample_helper.rb
+++ app/helpers/sample_helper.rb
@@ -1,11 +1,11 @@
 module SampleHelper
 	# Multi line R command with rinruby in sample_helper.rb
 	def r_script
-		require 'rinruby'
-  		r =  RinRuby.new # establishing a new RinRuby connection
-  		r.eval << EOF
-  			x <- 12345
-  			return(x)
- 		EOF
+		#require 'rinruby'
+  		#r =  RinRuby.new # establishing a new RinRuby connection
+  		#r.eval << EOF
+  			#x <- 12345
+  			#return(x)
+ 		#EOF
 	end 
 end 
