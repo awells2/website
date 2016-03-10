7529266b1ce6a89d397d554bb863068505dd3a41
awells2 <awells2@stanford.edu>
2016-03-09 22:52:21 -0800

    changed gem file

--- db/structure.sql
+++ db/structure.sql
@@ -40,3 +40,6 @@ INSERT INTO schema_migrations (version) VALUES ('20150624174713');
 INSERT INTO schema_migrations (version) VALUES ('20150624171207');
 
 INSERT INTO schema_migrations (version) VALUES ('20150624174713');
+INSERT INTO schema_migrations (version) VALUES ('20150624171207');
+
+INSERT INTO schema_migrations (version) VALUES ('20150624174713');
