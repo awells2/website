0e4e242da5f814c2d99ecef95608a9135ff4b5a4
awells2 <awells2@stanford.edu>
2016-03-14 23:45:45 -0700

    migrations

---
+++ db/migrate/20160315064311_experimentinfo.rb
@@ -0,0 +1,18 @@
+class Experimentinfo < ActiveRecord::Migration
+  def change
+	create_table: experimentinfo do |t|
+		t.integer :expId
+		t.string :expType
+		t.string :researcher
+		t.string :description
+		t.string :title
+		t.string :timeFormat
+		t.string :markers
+		t.string :cellLine
+		t.string :excelName	
+ 
+	end
+  end
+end
+
+
