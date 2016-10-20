7a92b42c990494cfb0483e858b4774ab42d45ee7
awells2 <awells2@stanford.edu>
2016-10-20 15:24:10 -0700

    search error

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -60,6 +60,7 @@ class SearchController < ApplicationController
 		
 		if(@target != "Any" and @pathway =="Any")
 			@info = Info.where('compoundName != ? and compoundName != ? and targetsEdited LIKE ?','Empty','Compound',@target).order(:compoundName).pluck(:compoundName)
+			puts @info
 			@targets = Info.where('compoundName != ? and compoundName != ?','Empty','Compound').order(:targetsEdited).pluck(:targetsEdited).uniq
 			@targetsParsed = []
 			@targets.each do |t|
